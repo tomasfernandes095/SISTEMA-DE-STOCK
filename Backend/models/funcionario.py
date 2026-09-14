67991245fc901356b72d969f57f6db3e497508eb
tomasfernandes095 <tomasfernandes095@gmail.com>
2026-09-14 15:15:55 +0100

    Criando a pasta dos Funcionários

---
+++ Backend/models/funcionario.py
@@ -0,0 +1,13 @@
+class Funcionario: 
+    nome: str
+    numero: int
+    cargo: str
+    ativo: bool
+    def __init__ (self, nome, numero_telemovel, cargo, ativo ):
+        self.nome = nome
+        self.numero = numero_telemovel
+        self.cargo = cargo
+        self.ativo = True
+
+
+
