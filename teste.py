463fc46341878525cdee378391546153256a6bca
tomasfernandes095 <tomasfernandes095@gmail.com>
2026-09-14 20:14:13 +0100

    deletando o teste.py e o teste02.py

--- teste.py
+++
@@ -1,7 +0,0 @@
-from Backend.services.produto_service import ProdutoService
-from Backend.database.database import cursor, banco_dados
-
-f= ProdutoService (cursor, banco_dados)
-
-f.Alterar_produto ()
-
