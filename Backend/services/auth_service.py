7dfbf6c0ce29c1326eb869585d7c5c1cd39ffd97
tomasfernandes095 <tomasfernandes095@gmail.com>
2026-09-17 22:15:55 +0100

    Melhoramento na autenticação dos funcionários

---
+++ Backend/services/auth_service.py
@@ -0,0 +1,20 @@
+from Backend.models.funcionario import Funcionario
+
+
+class Funcionários:
+    def __init__ (self, cursor, conexao):
+        self.cursor = cursor
+        self.conexao = conexao
+
+
+    def registar_utilizador (self):
+        nome = input('Qual o teu nome: ')
+        email = input('Qual o seu email: ')
+
+    def login (self):
+
+        
+        
+
+        
+
